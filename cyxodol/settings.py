5adcaaad1b2e1f0f228da818772ebc2acdf41a6b
mskornilov <Mell1990ad@gmail.com>
2018-09-12 16:57:41 +0300

    cyxodol, 4th commit

--- cyxodol/settings.py
+++ cyxodol/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-from settings_secret import SECRET_KEY, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
 import os
 
 
